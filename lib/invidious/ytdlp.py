eea86b2e5c59634076ab783d8b66a61abedde406
lekma <lekmalek@gmail.com>
2024-09-05 13:40:47 +0100

    better fps limit setting

--- lib/invidious/ytdlp.py
+++ lib/invidious/ytdlp.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+from collections import OrderedDict
+
 from iapc import AddonNotAvailable, Client
 from nuttig import getSetting, localizedString, okDialog
 
@@ -12,12 +14,18 @@ class YtDlp(object):
 
     __service_id__ = "service.yt-dlp"
 
+    __fps_limits__ = OrderedDict(((0, 40331), (30, 40332)))
+
     def __init__(self, logger):
         self.logger = logger.getLogger(f"{logger.component}.yt-dlp")
 
     def __setup__(self):
-        self.__fps_limit_30__ = getSetting("fps.limit.30", bool)
-        self.logger.info(f"{localizedString(40321)}: {self.__fps_limit_30__}")
+        self.__enabled__ = getSetting("context.fromyoutube", bool)
+        self.__fps__ = getSetting("fps.limit", int) if self.__enabled__ else 0
+        self.logger.info(
+            f"{localizedString(40321)}: "
+            f"{localizedString(self.__fps_limits__[self.__fps__])}"
+        )
 
     def __stop__(self):
         pass
@@ -25,10 +33,11 @@ class YtDlp(object):
     # play ---------------------------------------------------------------------
 
     def play(self, videoId):
-        fps = 30 if self.__fps_limit_30__ else 0
-        try:
-            return Client(self.__service_id__).play(
-                f"https://www.youtube.com/watch?v={videoId}", fps=fps
-            )
-        except AddonNotAvailable:
-            okDialog(localizedString(90004).format(self.__service_id__))
+        if self.__enabled__:
+            try:
+                return Client(self.__service_id__).play(
+                    f"https://www.youtube.com/watch?v={videoId}",
+                    fps=self.__fps__
+                )
+            except AddonNotAvailable:
+                okDialog(localizedString(90004).format(self.__service_id__))
