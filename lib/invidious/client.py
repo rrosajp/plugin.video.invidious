a93b40e603e1d8450305fca68f7421449247ae23
lekma <lekmalek@gmail.com>
2024-09-11 12:35:30 +0100

    cleanup

--- lib/invidious/client.py
+++ lib/invidious/client.py
@@ -40,7 +40,6 @@ class IVClient(object):
 
     @instance
     def play(self, **kwargs):
-        self.logger.info(f"play(kwargs={kwargs})")
         if (video := self.__client__.play(**kwargs)):
             return (Video(video).makeItem(video["url"]), video["manifestType"])
         return (None, None)
@@ -49,13 +48,11 @@ class IVClient(object):
 
     @instance
     def tabs(self, **kwargs):
-        self.logger.info(f"tabs(kwargs={kwargs})")
         if (tabs := self.__client__.tabs(**kwargs)):
             return Folders(tabs, **kwargs)
 
     @instance
     def tab(self, key, **kwargs):
-        self.logger.info(f"tab(key={key}, kwargs={kwargs})")
         videos = self.__client__.tab(key, **kwargs)
         return Videos(
             videos["videos"],
@@ -65,7 +62,6 @@ class IVClient(object):
 
     @instance
     def playlists(self, **kwargs):
-        self.logger.info(f"playlists(kwargs={kwargs})")
         playlists = self.__client__.playlists(**kwargs)
         return Playlists(
             playlists["playlists"],
@@ -77,7 +73,6 @@ class IVClient(object):
 
     @instance
     def playlist(self, **kwargs):
-        self.logger.info(f"playlist(kwargs={kwargs})")
         playlist = self.__client__.playlist(**kwargs)
         return Videos(
             playlist["videos"], limit=200, category=playlist["title"]
@@ -92,32 +87,27 @@ class IVClient(object):
 
     @instance
     def feed(self, limit=19, **kwargs):
-        self.logger.info(f"feed(kwargs={kwargs})")
         return Videos(self.__client__.feed.feed(limit, **kwargs), limit=limit)
 
     @instance
     def channels(self):
-        self.logger.info(f"channels()")
         return FeedChannels(self.__client__.feed.channels())
 
     # explore ------------------------------------------------------------------
 
     def explore(self):
-        self.logger.info(f"explore()")
         return Folders(self.__client__.folders("explore"))
 
     # popular ------------------------------------------------------------------
 
     @instance
     def popular(self, **kwargs):
-        self.logger.info(f"popular(kwargs={kwargs})")
         return Videos(self.__client__.popular(**kwargs), **kwargs)
 
     # trending -----------------------------------------------------------------
 
     @instance
     def trending(self, folders=False, **kwargs):
-        self.logger.info(f"trending(kwargs={kwargs})")
         if folders:
             return Folders(self.__client__.folders("explore", "trending"))
         return Videos(self.__client__.trending(**kwargs), **kwargs)
@@ -125,17 +115,14 @@ class IVClient(object):
     # search -------------------------------------------------------------------
 
     def query(self):
-        self.logger.info(f"query()")
         return self.__client__.search.query()
 
 
     def history(self):
-        self.logger.info(f"history()")
         return Queries(self.__client__.search.history())
 
     @instance
     def search(self, query):
-        self.logger.info(f"search(query={query})")
         return MixBag(
             self.__client__.search.search(query), limit=20, category=query["q"]
         )
